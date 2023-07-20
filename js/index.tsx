7f5040b80ef5036b94535b515181e35c74e35e25
Ian Davies <git@iandavies.org>
2023-07-20 18:21:56 +0100

    Update JS

--- js/index.tsx
+++ js/index.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
 import Button from 'react-bootstrap/Button';
 import { registerReactComponent } from '@anvil-works/anvil-react';
+import { useDesignerApi } from "@anvil-works/anvil-react/designer";
 
 import "../scss/theme.scss";
 
 registerReactComponent({
     name: "Bootstrap.Button",
     showInToolbox: true,
-    properties: [],
+    properties: [{
+        name: "text",
+        type: "string",
+        defaultValue: '',
+    }],
     events: [],
     component: ({
-        properties,
+        properties: {text},
     }, ref) => {
-        return <Button>A Button</Button>
+        const { inDesigner, designName } = useDesignerApi();
+        return <Button>{text || designName}</Button>
     }
 })
